31ad9effcfdafd6bf9b6a41cb8a1577c4e469b98
li-hang1 <lh2038672150@gmail.com>
2025-11-30 19:47:46 +0800

    updata

--- train.py
+++ train.py
@@ -14,10 +14,25 @@ from utils.evaluator import Evaluator
 from utils.setup_criterion import get_criterion
 from utils.trainer import Trainer
 
+import torch.distributed as dist
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 def main(config: omegaconf.DictConfig):
 
     os.environ["CUDA_VISIBLE_DEVICES"] = str(config.train.cuda_visible_device)
+    # os.environ["CUDA_VISIBLE_DEVICES"]是设置当前Python进程能看到哪些GPU的环境变量。
+    # 如果os.environ["CUDA_VISIBLE_DEVICES"]="2,3"，那么程序里torch.cuda.device_count()会返回2
+    rank = int(os.environ.get('RANK', '0'))
+    world_size = int(os.environ.get('WORLD_SIZE', '1'))
+    local_rank = int(os.environ.get('LOCAL_RANK', '0'))
+    dist.init_process_group(backend='nccl', init_method='env://', rank=rank, world_size=world_size)
+    torch.cuda.set_device(local_rank)
+    device = torch.device('cuda', local_rank)
+    is_main = (rank == 0)
+    if is_main:
+        print(f'[Main] world_size={world_size} backend = "nccl" ')
+
     save_path = prepare_logging()
 
     logging.info(f"Starting training at {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
@@ -25,6 +40,7 @@ def main(config: omegaconf.DictConfig):
     logging.info(f"Config file: {OmegaConf.to_yaml(config)}")
 
     train_dataset, valid_dataset, test_dataset = get_datasets(config.data)
+    train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank, shuffle=True)
 
     msg = f"Training with {len(train_dataset)} image pairs"
     if valid_dataset is not None:
@@ -32,17 +48,17 @@ def main(config: omegaconf.DictConfig):
     msg += f" and testing with {len(test_dataset)} image pairs."
     logging.info(msg)
 
-    train_loader = DataLoader(train_dataset, batch_size=config.train.batch_size, shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=config.train.batch_size, sampler=train_sampler, num_workers=os.cpu_count(), pin_memory=True)
     if valid_dataset is not None:
         valid_loader = DataLoader(valid_dataset, batch_size=1, shuffle=False)
     else:
         valid_loader = None
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-    model = NamedCurves(config.model)
+    model = NamedCurves(config.model).to(device)
+    model = DDP(model, device_ids=[local_rank])
     if config.model.ckpt_path is not None:
-        model.load_state_dict(torch.load(config.model.ckpt_path)["model_state_dict"])
-    model.cuda()
+        model.module.load_state_dict(torch.load(config.model.ckpt_path)["model_state_dict"])  # model.load_state_dict参数是字典
 
     criterion = get_criterion(config.train.criterion)
 
